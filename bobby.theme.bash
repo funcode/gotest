66c70ba39f04a5ad9fc986c91ddc483236a957c8
Teng Zhang and Wenlin Zhang <pairs+tezhang+wzhang@pivotal.io>
2018-01-14 19:23:37 +0800

    pivotal bobby theme

--- bobby.theme.bash
+++ bobby.theme.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-SCM_THEME_PROMPT_PREFIX=" ${green}|"
+SCM_THEME_PROMPT_PREFIX=" |"
 SCM_THEME_PROMPT_SUFFIX="${green}|"
 
 GIT_THEME_PROMPT_DIRTY=" ${red}✗"
@@ -13,42 +12,10 @@ GIT_THEME_PROMPT_SUFFIX="${green}|"
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
-__bobby_clock() {
-  printf "$(clock_prompt) "
-
-  if [ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]; then
-    printf "$(clock_char) "
-  fi
-}
-
-# Steal from demula theme
-mitsuhikos_lastcommandfailed() {
-  code=$?
-  if [ $code != 0 ];
-  then
-    echo "${white}exited ${red}\
-$code ${normal}"
-  fi
-}
+# This Pivotal's customization of the bobby theme
 
 function prompt_command() {
-    local LAST_COMMAND_FAILED=$(mitsuhikos_lastcommandfailed)
-    case $TERM in
-      xterm*)
-          TITLEBAR="\033]0;\W\007"
-          ;;
-      *)
-          TITLEBAR=""
-          ;;
-    esac
-
-   #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="${TITLEBAR}\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w ${LAST_COMMAND_FAILED}\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+    PS1="\n$(battery_char) ${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
 }
 
-THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
-THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
-THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
-THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M:%S"}
-
-safe_append_prompt_command prompt_command
+PROMPT_COMMAND=prompt_command;
